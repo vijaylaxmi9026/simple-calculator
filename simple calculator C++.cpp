6bfed906584ad88fd239d754ef6355d2700b5cc3
vijaylaxmi9026 <165749525+vijaylaxmi9026@users.noreply.github.com>
2024-04-02 09:30:24 +0530

    Add files via upload

---
+++ simple calculator C++.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char op;
+    float num1,num2;
+    cout << " Enter operation : +,-,*,/: ";
+    cin >> op;
+
+    cout << " Enter two operation : ";
+    cin >> num1 >> num2;
+
+    switch(op)
+    {
+        case '+' : 
+             cout << num1 << " + " << num2 << " = " << num1 + num2;
+             break;
+
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2;
+            break;
+
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2;
+            break;
+
+        case '/':
+            cout << num1 << " / " << num2 << " = " << num1 / num2 ;
+            break;
+
+        default :
+             cout << " Error! operation is not correct ";
+             break;
+    }
+        return 0;
+
+}
+
+        
+
+    
+
